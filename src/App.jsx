51a4d8ac2c1f806b7218cf07f1e1a91ec3f2930a
sanjiv.sahani <sanjiv.sahani@prolitus.com>
2022-08-14 21:37:48 +0530

    stage

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello Git </h1>
+      <h2>git init</h2>
+      Create empty Git repo in specified directory. Run with no
+      arguments to initialize the current directory as a git repository
+    </>
+  )
+}
+
+export default App
